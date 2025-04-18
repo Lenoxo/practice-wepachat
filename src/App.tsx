94cbee0a538d7d47374f4b8494dec6da7394ac89
Emanuel Padilla <emanuehl159@gmail.com>
2025-04-17 20:46:53 -0500

    Add prettier config files and format all files

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-import './App.css'
-import { Chats } from './Chats/Chats'
+import './App.css';
+import { Chats } from './Chats/Chats';
 
 function App() {
-  return (
-    <main>
-    <Chats />
-      <h2 className="welcome__title">
-      <img src='../tempImages/wepa.png' alt='icon' />
-      <p>
-        WepaChat
-      </p>
-      </h2>
-    </main>
-  )
+    return (
+        <main>
+            <Chats />
+            <h2 className="welcome__title">
+                <img src="../tempImages/wepa.png" alt="icon" />
+                <p>WepaChat</p>
+            </h2>
+        </main>
+    );
 }
 
-export default App
+export default App;
--- src/Chats/ChatItem/index.tsx
+++ src/Chats/ChatItem/index.tsx
@@ -1,5 +1,14 @@
-import './index.css'
+import './index.css';
 
 export function ChatItem() {
-    return <div className="chatItem"><img className='chatItem__img' src="../../tempImages/profile-default.svg" alt="contact" /><p>Romeo</p></div>
+    return (
+        <div className="chatItem">
+            <img
+                className="chatItem__img"
+                src="../../tempImages/profile-default.svg"
+                alt="contact"
+            />
+            <p>Romeo</p>
+        </div>
+    );
 }
