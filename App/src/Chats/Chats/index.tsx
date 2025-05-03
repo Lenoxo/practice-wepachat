35d232664760f996b71577869681b0edfc220f5e
Emanuel Padilla <emanuehl159@gmail.com>
2025-05-02 19:45:03 -0500

    Add dynamic render for profile data in Chats component

--- App/src/Chats/Chats/index.tsx
+++ App/src/Chats/Chats/index.tsx
@@ -1,16 +1,19 @@
+import { useContext } from 'react';
 import { ChatItem } from '../ChatItem';
 import './index.css';
+import { profilesContext } from '../../context';
 
 export function Chats() {
+    const context = useContext(profilesContext);
     return (
         <aside className="chats">
             <h2>Chats</h2>
             <div className="chatList">
-                <ChatItem />
-                <ChatItem />
-                <ChatItem />
-                <ChatItem />
-                <ChatItem />
+                {context?.map((profile) => {
+                    return (
+                        <ChatItem key={profile.id} imgLink={profile.image} name={profile.name} />
+                    );
+                })}
             </div>
         </aside>
     );
--- App/src/context/index.tsx
+++ App/src/context/index.tsx
@@ -1,27 +1,4 @@
-import { Children, createContext } from 'react';
+import { createContext } from 'react';
 import { Profile } from '../types';
 
-const profilesContext = createContext<Profile[]>([
-    {
-        id: 'a23',
-        name: 'Willy',
-        description: 'Is a youtuber',
-        image: 'https://cdn.pfps.gg/pfps/9479-yuji-itadori.png'
-    },
-    {
-        id: 'a24',
-        name: 'Willy',
-        description: 'Is a youtuber',
-        image: 'https://cdn.pfps.gg/pfps/9479-yuji-itadori.png'
-    },
-    {
-        id: 'a25',
-        name: 'Willy',
-        description: 'Is a youtuber',
-        image: 'https://cdn.pfps.gg/pfps/9479-yuji-itadori.png'
-    }
-]);
-
-export function profliesProvider() {
-    return <profilesContext.Provider>{Children}</profilesContext.Provider>;
-}
+export const profilesContext = createContext<Profile[]>([]);
