7199c9da0c980bc9a40cc54bdfaf837b9fd771e8
Emanuel Padilla <emanuehl159@gmail.com>
2025-05-23 15:23:10 -0500

    Add ProfileInfo component and render it using a portal

--- App/src/Chats/Chats/index.tsx
+++ App/src/Chats/Chats/index.tsx
@@ -1,12 +1,18 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { ChatItem } from '../ChatItem';
 import './index.css';
 import { profilesContext } from '../../context';
+import { createPortal } from 'react-dom';
+import { ProfileInfo } from '../../Profile/ProfileInfo';
 
 export function Chats() {
     const context = useContext(profilesContext);
+    const [openModal, setOpenModal] = useState<boolean>(false);
+
     return (
         <aside className="chats">
+            <button onClick={() => setOpenModal(true)}>⚙️</button>
+            {openModal && createPortal(<ProfileInfo setOpenModal={setOpenModal} />, document.body)}
             <h2>Chats</h2>
             <div className="chatList">
                 {context?.map((profile) => {
