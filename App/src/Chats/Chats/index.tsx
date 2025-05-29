a9ee2f4917f4508d19fb4952637fb675f3b6b87d
Emanuel Padilla <emanuehl159@gmail.com>
2025-05-28 21:02:39 -0500

    Add working filtering for SearchBar in Chats component

--- App/src/Chats/Chats/index.tsx
+++ App/src/Chats/Chats/index.tsx
@@ -6,16 +6,37 @@ import { createPortal } from 'react-dom';
 import { ProfileInfo } from '../../Profile/ProfileInfo';
 
 export function Chats() {
-    const context = useContext(profilesContext);
+    const profiles = useContext(profilesContext);
     const [openModal, setOpenModal] = useState<boolean>(false);
+    const [filteredProfiles, setFilteredProfiles] = useState(profiles);
+
+    function handleSearchInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+        // I use toLowerCase because it makes filtering matching easier, rather that being case sensitive
+        const value = event.target.value.toLowerCase();
+
+        console.info(value);
+
+        const updatedProfiles = profiles.filter((profile) => {
+            if (profile.name.toLowerCase().includes(value)) {
+                return profile;
+            }
+        });
+
+        setFilteredProfiles(updatedProfiles);
+    }
 
     return (
         <aside className="chats">
+            <input
+                type="search"
+                placeholder="Search by contact name"
+                onChange={handleSearchInputChange}
+            />
             <button onClick={() => setOpenModal(true)}>⚙️</button>
             {openModal && createPortal(<ProfileInfo setOpenModal={setOpenModal} />, document.body)}
             <h2>Chats</h2>
             <div className="chatList">
-                {context?.map((profile) => {
+                {filteredProfiles?.map((profile) => {
                     return (
                         <ChatItem key={profile.id} imgLink={profile.image} name={profile.name} />
                     );
