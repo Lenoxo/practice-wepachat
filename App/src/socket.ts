27e2af5660aa994668bdd07721d686ebcaafcb17
Emanuel Padilla <emanuehl159@gmail.com>
2025-05-29 20:29:25 -0500

    Add env var and read it for the local url

--- App/src/socket.ts
+++ App/src/socket.ts
@@ -1,8 +1,9 @@
 import { io } from 'socket.io-client';
 
-// "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:3000';
+// const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:3000';
 
-console.info(URL)
+// "undefined" means the URL will be computed from the `window.location` object
+const URL = process.env.NODE_ENV === 'production' ? undefined : import.meta.env.VITE_URL_WS;
+// You would need to create an .env.local file with the url for VITE_URL_WS
 
 export const socket = io(URL);
