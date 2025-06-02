38fd3aa7228d5de11b7868d464d27c4736415901
Emanuel Padilla <emanuehl159@gmail.com>
2025-06-01 22:49:00 -0500

    WIP render received messages in ViewMessage

--- App/src/Chats/ViewMessage/index.tsx
+++ App/src/Chats/ViewMessage/index.tsx
@@ -1,22 +1,40 @@
+import { useEffect, useState } from 'react';
 import { socket } from '../../socket';
 
 export function ViewMessage() {
-    const messagesSection = document.getElementById('messages__list');
+    const [messages, setMessages] = useState<Array<string>>([]);
 
-    socket.on('chat message', (msg) => {
-        const item = document.createElement('li');
-        item.textContent = msg;
-        if (messagesSection === null) {
-            throw new Error(
-                'messagesSection is null, check if you are selecting the right element'
-            );
+    useEffect(() => {
+        function updateMessages(msg: string) {
+            const newMessages = [...messages, msg];
+            setMessages(newMessages);
         }
-        messagesSection.appendChild(item);
-        window.scrollTo(0, document.body.scrollHeight);
-    });
+        socket.on('chat message', updateMessages);
+
+        // socket.off('chat message');
+    }, []);
+    // socket.on('chat message', (msg) => {
+    //     console.warn(msg);
+    // const messagesSection = document.getElementById('messages__list');
+
+    // console.count(msg);
+    // const item = document.createElement('li');
+    // item.textContent = msg;
+
+    // if (messagesSection === null) {
+    //     throw new Error('messagesSection is null, check if you are selecting the right element');
+    // }
+    // messagesSection.appendChild(item);
+    // window.scrollTo(0, document.body.scrollHeight);
+    // });
+
     return (
         <section className="messages">
-            <ul id="messages__list"></ul>
+            <ul id="messages__list">
+                {messages.map((msg, index) => {
+                    return <li key={index}>{msg}</li>;
+                })}
+            </ul>
         </section>
     );
 }
